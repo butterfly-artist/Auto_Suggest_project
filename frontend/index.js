0e1111437a899bd925134deb38f9a8c5f5a3382b
Thotamadhulika <thota.madhulika05@gmail.com>
2026-07-04 10:02:48 +0530

    random button

--- frontend/index.js
+++ frontend/index.js
@@ -21,6 +21,35 @@ function toggle(){
   document.getElementById("img").src=users[curUserId].image;
   // name
   document.getElementById("card-name").innerText=users[curUserId].name;
+  document.getElementById("card-gender").innerText=users[curUserId].gender;
 }
 
 console.log("Hello from JS");
+ 
+function getRandomUser()
+{
+  fetch("https://randomuser.me/api")
+    .then(function (data){
+      return data.json()
+    })
+    .then(function (parsedData){
+      // console.log(parsedData)
+      let gender = parsedData.results[0].gender;
+
+      let first = parsedData.results[0].name.first;
+      let last = parsedData.results[0].name.last;
+      let name = first  + " " + last;
+      let imageUrl = parsedData.results[0].picture.large;
+
+      // gender
+      document.getElementById("card-gender").innerText=gender;
+      // name
+      document.getElementById("card-name").innerText=name
+      // img
+      document.getElementById("img").src=imageUrl
+
+    })
+    // Add button to call this function
+    //document.getElementById("random-user-btn").addEventListener("click", getRandomUser);
+
+}
